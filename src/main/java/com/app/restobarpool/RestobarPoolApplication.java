ff991a89fdbf93330d2167a04c6b3f4de35c03fa
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-08-27 20:01:14 -0300

    1. Inicializacion de proyecto y primeras configuraciones

---
+++ src/main/java/com/app/restobarpool/RestobarPoolApplication.java
@@ -0,0 +1,13 @@
+package com.app.restobarpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestobarPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestobarPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/restobarpool/RestobarPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restobarpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestobarPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
